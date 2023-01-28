b06755b36623f087c3730258dd763d346377b47c
Arlindo Pinto <perensen@gmail.com>
2023-01-28 11:18:41 -0500

    Added conditional formating to text areas based on current hour

--- script.js
+++ script.js
@@ -2,51 +2,56 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  // Save button function to store entry to local storage when user clicks save button
   $('.saveBtn').click(function() {
     var btnClickId = $(this).parent().attr('id'); 
     var clickFieldValue = $.trim($(this).parent().children('textarea').val());
     if (clickFieldValue.length < 1) {
       $('#calendar-entry-error').modal('show')
-      //alert("Enter a valid entry.")
       return;
     }else {
       localStorage.setItem(btnClickId, clickFieldValue);
-      //console.log(localStorage.getItem(btnClickId));
     } 
   }); 
-    
-    $('.deleteBtn').click(function() {
-      //var btnClickId = localStorage.removeItem($(this).parent().attr('id')); 
-      var clickFieldValue = $.trim($(this).parent().children('textarea').val());
-      if (clickFieldValue.length < 1) {
-        alert("Nothing to Delete");
-        return;
+
+  // Delete button function to delete entry from local storage when user clicks delete button
+  $('.deleteBtn').click(function() {
+    var clickFieldValue = $.trim($(this).parent().children('textarea').val());
+    if (clickFieldValue.length < 1) {
+      alert("Nothing to Delete");
+      return;
+    }else {
+      var confirmDel = confirm("Are you sure you want to delete this entry?")
+      if(confirmDel === true) {
+        localStorage.removeItem($(this).parent().attr('id')); 
+        $(this).parent().children('textarea').val("");
       }else {
-        var confirmDel = confirm("Are you sure you want to delete?")
-        if(confirmDel === true) {
-          localStorage.removeItem($(this).parent().attr('id')); 
-          $(this).parent().children('textarea').val("");
-        }else {
-          return;
-        }
-      } 
+        return;
+      }
+    } 
   });
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  
+  // get current hour using dayjs
   var currentHour = dayjs().format('H');
-  console.log(currentHour);
+
+  // identify all time-blocks elements
+  $('.time-block').each(function() {
+    //gets the attribute from ID
+    var id = $(this).attr('id');
+    //splits string on "-" to return the hour value, string is converted to a number using parseInt() to be able to compare against hour
+    var hour = parseInt(id.split('-')[1]);
+    // compares hour from ID to current hour, adds class past, future or present to that div.
+    if (parseInt(hour) < currentHour) {
+      $(this).addClass("past");
+    } else if (parseInt(hour) > currentHour) {
+      $(this).addClass("future");
+    } else {
+      $(this).addClass("present");
+    }
+  });
+  
+  
   // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
   $('#hour-9 .description').val(localStorage.getItem('hour-9'));
   $('#hour-10 .description').val(localStorage.getItem('hour-10'));
   $('#hour-11 .description').val(localStorage.getItem('hour-11'));
@@ -55,6 +60,7 @@ $(function () {
   $('#hour-14 .description').val(localStorage.getItem('hour-14'));
   $('#hour-15 .description').val(localStorage.getItem('hour-15'));
   $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+  
   // TODO: Add code to display the current date in the header of the page.
   var currentDayEl = $('#currentDay');
   var currentDate = dayjs().format('MMMM D, YYYY');
